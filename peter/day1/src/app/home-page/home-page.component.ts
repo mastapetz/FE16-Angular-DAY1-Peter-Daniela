f789b535a327b11f8dc1fd1ad73ddde4cd61b782
Peter Aigner <mastapetz@icloud.com>
2022-06-13 14:33:58 +0200

    routing

--- peter/day1/src/app/home-page/home-page.component.ts
+++ peter/day1/src/app/home-page/home-page.component.ts
@@ -6,30 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  destinationName1: string ="Dreamlocation";
-  destinationPrice1: number = 500;
-  destinationNights1: string ="five";
-  destinationCity1: string ="Dreamlocation";
-  destinationName2: string ="Dreamlocation";
-  destinationPrice2: number = 500;
-  destinationNights2: string ="five";
-  destinationCity2: string ="Dreamlocation";
-  destinationName3: string ="Dreamlocation";
-  destinationPrice3: number = 500;
-  destinationNights3: string ="five";
-  destinationCity3: string ="Dreamlocation";
-  destinationName4: string ="Dreamlocation";
-  destinationPrice4: number = 500;
-  destinationNights4: string ="five";
-  destinationCity4: string ="Dreamlocation";
-  destinationName5: string ="Dreamlocation";
-  destinationPrice5: number = 500;
-  destinationNights5: string ="five";
-  destinationCity5: string ="Dreamlocation";
-  destinationName6: string ="Dreamlocation";
-  destinationPrice6: number = 500;
-  destinationNights6: string ="five";
-  destinationCity6: string ="Dreamlocation";
+  destinationName1: string ="Dreamlocation 1";
+  destinationPrice1: number = 1500;
+  destinationNights1: number =3;
+  destinationCity1: string ="Berlin";
+  destinationName2: string ="Dreamlocation 2";
+  destinationPrice2: number = 2500;
+  destinationNights2: number =5;
+  destinationCity2: string ="Frankfurt";
+  destinationName3: string ="Dreamlocation 3 ";
+  destinationPrice3: number = 3800;
+  destinationNights3: number =13;
+  destinationCity3: string ="Salzburg";
+  destinationName4: string ="Dreamlocation 4";
+  destinationPrice4: number = 4500;
+  destinationNights4: number =19;
+  destinationCity4: string ="Tokyo";
+  destinationName5: string ="Dreamlocation 5";
+  destinationPrice5: number = 440;
+  destinationNights5: number =4;
+  destinationCity5: string ="Vienna";
+  destinationName6: string ="Dreamlocation 6";
+  destinationPrice6: number = 120.000;
+  destinationNights6: number = 10;
+  destinationCity6: string ="Space Station Nebula X";
   constructor() { }
 
   ngOnInit(): void {
