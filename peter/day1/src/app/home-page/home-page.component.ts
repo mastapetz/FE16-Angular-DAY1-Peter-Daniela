15e1e6164ff1fb8f2e4ea46a39e8c804e637a586
Peter Aigner <mastapetz@icloud.com>
2022-06-13 14:21:55 +0200

    hero and home done

--- peter/day1/src/app/home-page/home-page.component.ts
+++ peter/day1/src/app/home-page/home-page.component.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
+  destinationName1: string ="Dreamlocation";
+  destinationPrice1: number = 500;
+  destinationNights1: string ="five";
+  destinationCity1: string ="Dreamlocation";
+  destinationName2: string ="Dreamlocation";
+  destinationPrice2: number = 500;
+  destinationNights2: string ="five";
+  destinationCity2: string ="Dreamlocation";
+  destinationName3: string ="Dreamlocation";
+  destinationPrice3: number = 500;
+  destinationNights3: string ="five";
+  destinationCity3: string ="Dreamlocation";
+  destinationName4: string ="Dreamlocation";
+  destinationPrice4: number = 500;
+  destinationNights4: string ="five";
+  destinationCity4: string ="Dreamlocation";
+  destinationName5: string ="Dreamlocation";
+  destinationPrice5: number = 500;
+  destinationNights5: string ="five";
+  destinationCity5: string ="Dreamlocation";
+  destinationName6: string ="Dreamlocation";
+  destinationPrice6: number = 500;
+  destinationNights6: string ="five";
+  destinationCity6: string ="Dreamlocation";
   constructor() { }
 
   ngOnInit(): void {
